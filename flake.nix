2f2b283affaa41c4ef0812045d4c3906c4d0d9ef
Brage Vik <brage.vik@lyse.net>
2026-09-01 11:05:14 +0200

    add a nix flake for development
    I doubt it will be relevant for anyone else than me

---
+++ flake.nix
@@ -0,0 +1,22 @@
+{
+  description = "Python environment with pandas";
+
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-26.05";
+
+  outputs = {nixpkgs, ...}: let
+    system = "x86_64-linux";
+    pkgs = nixpkgs.legacyPackages.${system};
+
+    python = pkgs.python3.withPackages (ps:
+      with ps; [
+        pandas
+		matplotlib
+      ]);
+  in {
+    devShells.${system}.default = pkgs.mkShell {
+      packages = [
+        python
+      ];
+    };
+  };
+}
