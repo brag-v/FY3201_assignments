1db427a6a360c31365d9b1bcd9c1e027796cfe96
Abel Horneland <abel.horneland@gmail.com>
2026-09-02 15:57:49 +0200

    Fixed figure size for the report

--- assignment_1/anomoly.py
+++ assignment_1/anomoly.py
@@ -6,14 +6,15 @@ import matplotlib.pyplot as plt
 data = pd.read_csv('./assignment_1/air_temperature_sea_ice.csv')
 
 font = {'size'   : 20}
-
 matplotlib.rc('font', **font)
     
 x = data['Year'] + (data['Month'] - 1) / 12
 
+plt.figure(figsize=(12, 7))
+
 plt.plot(x, data['Monthly_Anomaly'], label='Monthly Anomaly', color='orange', alpha=0.5)
 plt.plot(x, data['Annual_Anomaly'], label='Yearly Anomaly')
-plt.fill_between(x, data['Annual_Unc'] + data['Annual_Anomaly'], data['Annual_Anomaly'] - data['Annual_Unc'], alpha=0.5, label='95% Confidence Interval')
+plt.fill_between(x, data['Annual_Unc'] + data['Annual_Anomaly'], data['Annual_Anomaly'] - data['Annual_Unc'], alpha=0.5, label='Annual Uncertainty (95% CI)')
 plt.fill_between(x, -10, 10, where=(1951 <= data['Year']) & (data['Year'] <= 1980), color='grey', alpha=0.2, label='Baseline Period (1951-1980)')
 plt.xlabel('Year')
 plt.ylabel('Temperature Anomaly (°C)')
