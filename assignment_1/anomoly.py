aa2c2447a1606ff0459f9774d8dc4e27a59be46d
Abel Horneland <abel.horneland@gmail.com>
2026-09-02 14:21:12 +0200

    Font stuff

--- assignment_1/anomoly.py
+++ assignment_1/anomoly.py
@@ -13,8 +13,8 @@ x = data['Year'] + (data['Month'] - 1) / 12
 
 plt.plot(x, data['Monthly_Anomaly'], label='Monthly Anomaly', color='orange', alpha=0.5)
 plt.plot(x, data['Annual_Anomaly'], label='Yearly Anomaly')
-plt.fill_between(x, data['Annual_Unc'] + data['Annual_Anomaly'], data['Annual_Anomaly'] - data['Annual_Unc'], alpha=0.3, label='95% Confidence Interval')
-plt.fill_between(x, -10, 10, where=(1951 <= data['Year']) & (data['Year'] <= 1980), color='red', alpha=0.2, label='Baseline Period (1951-1980)')
+plt.fill_between(x, data['Annual_Unc'] + data['Annual_Anomaly'], data['Annual_Anomaly'] - data['Annual_Unc'], alpha=0.5, label='95% Confidence Interval')
+plt.fill_between(x, -10, 10, where=(1951 <= data['Year']) & (data['Year'] <= 1980), color='grey', alpha=0.2, label='Baseline Period (1951-1980)')
 plt.xlabel('Year')
 plt.ylabel('Temperature Anomaly (°C)')
 plt.title('Yearly Temperature Anomaly')
--- assignment_1/linear_warming_rate.py
+++ assignment_1/linear_warming_rate.py
@@ -1,3 +1,4 @@
+import matplotlib
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
@@ -7,6 +8,8 @@ data = pd.read_csv("./assignment_1/air_temperature_sea_ice.csv")
 data["date"] = pd.to_datetime(data[["Year", "Month"]].assign(day=15))
 data = data.set_index("date").sort_index()
 
+font = {'size'   : 20}
+matplotlib.rc('font', **font)
 
 r = data.loc["1998-01-01":"2024-01-01"]
 
@@ -19,10 +22,10 @@ fit = reg.predict(time.reshape(-1, 1))
 
 plt.plot(r.index, r["Annual_Anomaly"], label="Annual Anomaly")
 plt.plot(r.index, fit, label="Linear fit")
-plt.xlabel("Year", fontsize=20)
-plt.ylabel("Temperature Anomaly (°C)", fontsize=20)
-plt.title("Linear trend in temperature", fontsize=20)
+plt.xlabel("Year")
+plt.ylabel("Temperature Anomaly (°C)")
+plt.title("Linear trend in temperature")
 plt.ylim(-1.2, 1.7)
-plt.legend(fontsize=20)
+plt.legend()
 plt.grid()
 plt.show()
