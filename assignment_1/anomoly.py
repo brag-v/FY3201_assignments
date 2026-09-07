dff250e4a53decd56776a87b60137af3e872ac07
Abel Horneland <abel.horneland@gmail.com>
2026-09-07 13:08:03 +0200

    Cleanup anomoly

--- assignment_1/anomoly.py
+++ assignment_1/anomoly.py
@@ -5,7 +5,7 @@ import matplotlib.pyplot as plt
 
 data = pd.read_csv('./assignment_1/air_temperature_sea_ice.csv')
 
-font = {'size'   : 20}
+font = {'size'   : 18}
 matplotlib.rc('font', **font)
     
 x = data['Year'] + (data['Month'] - 1) / 12
@@ -13,12 +13,12 @@ x = data['Year'] + (data['Month'] - 1) / 12
 plt.figure(figsize=(12, 7))
 
 plt.plot(x, data['Monthly_Anomaly'], label='Monthly Anomaly', color='orange', alpha=0.5)
-plt.plot(x, data['Annual_Anomaly'], label='Yearly Anomaly')
+plt.plot(x, data['Annual_Anomaly'], label='Annual Anomaly')
 plt.fill_between(x, data['Annual_Unc'] + data['Annual_Anomaly'], data['Annual_Anomaly'] - data['Annual_Unc'], alpha=0.5, label='Annual Uncertainty (95% CI)')
 plt.fill_between(x, -10, 10, where=(1951 <= data['Year']) & (data['Year'] <= 1980), color='grey', alpha=0.2, label='Baseline Period (1951-1980)')
 plt.xlabel('Year')
 plt.ylabel('Temperature Anomaly (°C)')
-plt.title('Yearly Temperature Anomaly')
+plt.title('Annual Temperature Anomaly')
 plt.ylim(-1.2, 1.7)
 plt.legend()
 plt.grid()
